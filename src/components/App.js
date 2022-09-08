f20bb0931428da283944dd057d946a2201f9fe49
Your Name <lucasdosualdo@gmail.com>
2022-09-08 16:39:30 -0300

    create project

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import GlobalStyle from "../styles/globalStyle"
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import MyProfile from './MyProfile';
+import Input from './Input';
+import Output from './Output';
+
+export default function App() {
+    return (
+        <>
+        <GlobalStyle/>
+        <BrowserRouter>
+            testando
+            <Routes>
+                <Route path='/' element={<SignIn/>}/>
+                <Route path='/signup' element = {<SignUp/>}/>
+                <Route path='/myprofile' element = {<MyProfile/>}/>
+                <Route path = '/input' element = {<Input/>}/>
+                <Route path = '/output' element = {<Output/>}/>
+            </Routes>
+        </BrowserRouter>
+        </>
+    )
+}
---
+++ src/components/Output.js
@@ -0,0 +1,7 @@
+export default function OutPut() {
+    return (
+        <>
+        
+        </>
+    )
+}
